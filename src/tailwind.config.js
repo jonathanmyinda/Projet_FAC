a3b3a7e8c180eb5b7c1c818e2d7bde32e37c8f72
Jonathan Myinda <myindaj@gmail.com>
2025-12-07 20:24:52 +0100

    chore: init Vite + React + Tailwind setup (config & base structure)

---
+++ src/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+content: ["./index.html", "./src/**/*.{js,jsx}"] ,
+theme: {
+extend: {
+colors: {
+forest: "#2B4D1E",
+earth: "#A97142",
+clay: "#D9A679"
+}
+},
+},
+plugins: [],
+};
